77b55252dbca2be9091b45c34746ffbbbce81025
lizavetaklimovich <60734453+LizavetaKlimovich@users.noreply.github.com>
2022-12-11 17:03:22 +0100

    days 10-11

---
+++ day10.py
@@ -0,0 +1,58 @@
+def parse(filepath):
+    with open(filepath) as f:
+        full_list = []
+        for line in f:
+            full_list.append(line.strip().split(" "))
+            if line.startswith("addx"):
+                full_list.append(["wait"])
+
+    return full_list
+
+
+def part1(parsed):
+    x = 1
+    termin = 0
+    total = 0
+    for cycle, command in enumerate(parsed):
+
+        if cycle == 220:
+            break
+        if command[0] == "nood":
+            continue
+        if command[0] == "addx":
+            termin = int(command[1])
+
+        if cycle in (19, 59, 99, 139, 179, 219):
+            total = total + (x * (cycle + 1))
+
+        if command[0] == "wait":
+            x += termin
+            termin = 0
+    return total
+
+
+if __name__ == "__main__":
+    parsed = parse("./day10.txt")
+    x = 1
+    termin = 0
+    pixel = (x-1, x, x+1)
+    line = ["."]*40
+
+    with open("res.txt", "w") as out:
+        for cycle, command in enumerate(parsed):
+            if (cycle+1)%40 == 0:
+                out.write(''.join(line))
+                out.write("\n")
+                line = ["."]*40
+
+            if command[0] == "addx":
+                termin = int(command[1])
+
+            if cycle%40 in pixel:
+                line[cycle%40] = "#"
+
+            if command[0] == "wait":
+                x += termin
+                pixel = (x - 1, x, x + 1)
+
+    print(part1(parsed))
---
+++ day11.py
@@ -0,0 +1,51 @@
+import copy
+import math
+from collections import namedtuple, Counter
+import re
+from functools import reduce
+
+Monkey = namedtuple("Monkey", "items,operation,test,if_true,if_false")
+
+
+def parse(filepath):
+    monkeys = []
+
+    f = iter(open(filepath).read().strip("\n").split("\n"))
+
+    while True:
+        try:
+            x = next(f)
+        except StopIteration:
+            break
+
+        if x.startswith("Monkey"):
+            items = [int(i) for i in re.findall(r"\d+", next(f))]
+            op = next(f).split("old ")[-1].split(" ")
+            test = [int(i) for i in re.findall(r"\d+", next(f))][0]
+            if_true = [int(i) for i in re.findall(r"\d+", next(f))][0]
+            if_false = [int(i) for i in re.findall(r"\d+", next(f))][0]
+            monkey = Monkey(items, op, test, if_true, if_false)
+            monkeys.append(monkey)
+    return monkeys
+
+
+if __name__ == "__main__":
+    monkeys = parse("day11.txt")
+    common_div = reduce(lambda x, y: x*y, [m.test for m in monkeys])
+    counter = Counter()
+    for i in range(10000):
+        for monkey in monkeys:
+            for j, item in enumerate(copy.deepcopy(monkey.items)):
+                op = item if monkey.operation[1] == "old" else int(monkey.operation[1])
+                worry_level = item+op if monkey.operation[0] == "+" else item*op
+                # part 1:
+                # new_wl = math.floor(worry_level/3)
+                new_wl = worry_level
+                if new_wl % monkey.test == 0:
+                    monkeys[monkey.if_true].items.append(new_wl%common_div)
+                else:
+                    monkeys[monkey.if_false].items.append(new_wl%common_div)
+                del monkey.items[0]
+                counter[monkey.test] += 1
+
+    print(counter.most_common(2)[0][1] * counter.most_common(2)[1][1])
